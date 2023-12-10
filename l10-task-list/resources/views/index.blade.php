6310fd857e8f2bf38ef2219dbe2dcdfbcaafde82
danielk_kruger <kruger.dkk@gmail.com>
2023-12-10 18:26:25 -0300

    Final Project

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -3,12 +3,26 @@
 @section('title', 'The List of tasks')
 
 @section('content')
+    <nav class="mb-4">
+        <a class="link" href="{{ route('tasks.create') }}">Add Task </a>
+    </nav>
 
     @forelse($tasks as $task)
-        <div><a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a></div>
+        <div>
+            <a href="{{ route('tasks.show', ['task' => $task->id]) }}" @class(['line-through' => $task->completed])>
+                {{ $task->title }}
+            </a>
+        </div>
     @empty
         <div>Nothing to show here</div>
     @endforelse
+
+    @if($tasks->count())
+        <nav class="mt-4">
+            {{ $tasks->links() }}
+        </nav>
+
+    @endif
 
 @endsection
 
